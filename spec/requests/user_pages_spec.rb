2d96750e5826dfe09b2ba1be59937918b7ff4eb8
vantubbe <mike.vantubbe@gmail.com>
2014-05-09 00:13:10 -0500

    Adding User Administration

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -64,5 +64,44 @@ describe "UserPages" do
     it {should have_content(user.name)}
   end
 
+  describe 'edit' do
+    let(:user) {FactoryGirl.create(:user)}
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it {should have_content('Update your profile')}
+      it {should have_title('Edit User')}
+      it {should have_link('change', href: 'http://gravatar.com/emails')}
+    end
+
+    describe "with invalid infomation" do
+      before {click_button "Save changes"}
+      it{should have_content 'error'}
+    end
+
+    describe 'with valid information' do
+      let(:new_name) {'New Name'}
+      let(:new_email) {'new@example.com'}
+      before do
+        fill_in "Name",         with: new_name
+        fill_in "Email",        with: new_email
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+        click_button "Save changes"
+      end
+
+      it {should have_title(new_name)}
+      it {should have_selector('div.alert.alert-success', text: 'updated') }
+      it {should have_link('Sign out', signout_path)}
+      specify {expect(user.reload.name).to eq new_name }
+      specify {expect(user.reload.email).to eq new_email }
+
+    end
+
+  end
+
 end
 
